531dcecd14c7630c320cce905b6838feaf9cb657
evgzhuravlev <evgzhuravlev@yandex-team.ru>
2022-10-03 12:56:50 +0300

    mind_games_5(update_1)

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,28 +8,28 @@ public class App {
         String greet =
                 "Please enter the game number and press Enter.\n"
                         + "1 - Greet\n"
-                        + "2 - Even\n "
+                        + "2 - Even\n"
                         + "0 - Exit";
 
         System.out.println(greet);
         String selectNumber = sc.next();
+        choicePrint(selectNumber);
         if ("1".equals(selectNumber)) {
-            System.out.println("Your choice: 1");
             Cli.sayHello(sc);
             main(args);
         } else if ("2".equals(selectNumber)) {
-            System.out.println("Your choice: 2");
             String name = Cli.sayHello(sc);
             CheckForEven.game(sc, name);
             main(args);
         } else if ("0".equals(selectNumber)) {
-            System.out.println("Your choice: 0\n"
-                    + "Exit");
             sc.close();
         } else {
             System.out.println("Please enter a valid game number.");
             main(args);
         }
         sc.close();
+    }
+    public static void choicePrint(String choice) {
+        System.out.println("Your choice: " + choice);
     }
 }
--- app/src/main/java/hexlet/code/CheckForEven.java
+++ app/src/main/java/hexlet/code/CheckForEven.java
@@ -6,38 +6,38 @@ import static java.lang.Math.random;
 
 public class CheckForEven {
     public static void game(Scanner sc, String name) {
-        String msgResult;
+
         System.out.println("Answer 'yes' if number even otherwise answer 'no'.");
-        final int step = 3; // шагов цикла
-        final int maxNumber = 100; // максимальное рандомное
+        final int step = 3; // кол-во шагов в игре
+        gameCount(sc, name, step);
+    }
+
+    public static void gameCount(Scanner sc, String name, int step) {
+        String msgResult;
         for (int i = 1; i <= step; i++) {
-            int randomNumber = (int) (random() * maxNumber);
-            System.out.println("Question: " + randomNumber);
-            String answer = sc.next();
-            System.out.println("Your answer: " + answer);
-
-            if ((randomNumber % 2 == 0 && "yes".equals(answer)) || (randomNumber % 2 != 0 && "no".equals(answer))) {
-                msgResult = "Correct!";
-            } else {
-                String correctAnswer;
-                if ("yes".equals(answer)) {
-                    correctAnswer = "no";
-                } else {
-                    correctAnswer = "yes";
-                }
-                msgResult = "'" + answer + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'.\n"
-                        + "Let's try again, " + name + "!";
-            }
-
+            msgResult = gameCondition(sc, name);
             System.out.println(msgResult);
-
             if (!"Correct!".equals(msgResult)) {
                 break;
             }
-
             if (i == step) {
                 System.out.println("Congratulations, " + name + "!");
             }
+        }
+    }
+
+    public static String gameCondition(Scanner sc, String name) {
+        final int maxNumber = 100; // максимальное рандомное
+        int randomNumber = (int) (random() * maxNumber);
+        System.out.println("Question: " + randomNumber);
+        String answer = sc.next();
+        System.out.println("Your answer: " + answer);
+        if ((randomNumber % 2 == 0 && "yes".equals(answer)) || (randomNumber % 2 != 0 && "no".equals(answer))) {
+            return "Correct!";
+        } else {
+            String correctanswer = "yes".equals(answer) ? "no" : "yes";
+            return "'" + answer + "' is wrong answer ;(. Correct answer was '" + correctanswer + "'.\n"
+                    + "Let's try again, " + name + "!";
         }
     }
 }
