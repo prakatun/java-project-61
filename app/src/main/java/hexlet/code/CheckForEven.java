8b4f94887e1002b6676e47e24a16951bee3bdb15
evgzhuravlev <evgzhuravlev@yandex-team.ru>
2022-10-03 14:36:32 +0300

    mind_games_5(update_6)

--- app/src/main/java/hexlet/code/CheckForEven.java
+++ app/src/main/java/hexlet/code/CheckForEven.java
@@ -22,35 +22,20 @@ public class CheckForEven {
         }
     }
 
-    public static int gameRandomNumber() {
+    public static boolean gameCondition(Scanner sc, String name) {
         final int maxNumber = 100; // максимальное рандомное
-        int randomnumber = (int) (random() * maxNumber);
-        System.out.println("Question: " + randomnumber);
-        return randomnumber;
-    }
-
-    public static String gamePrintNumber(Scanner sc) {
+        int randomNumber = (int) (random() * maxNumber);
+        System.out.println("Question: " + randomNumber);
         String answer = sc.next();
         System.out.println("Your answer: " + answer);
-        return answer;
-    }
-    public static boolean gamePrintCorrect() {
-        System.out.println("Correct!");
-        return true;
-    }
-
-    public static boolean gameCondition(Scanner sc, String name) {
-        int randomNumber = gameRandomNumber();
-        String answer = gamePrintNumber(sc);
-        if (randomNumber % 2 == 0 && "yes".equals(answer)) {
-            return gamePrintCorrect();
-        } else if (randomNumber % 2 != 0 && "no".equals(answer)) {
-            return gamePrintCorrect();
-        } else {
-            String correctanswer = "yes".equals(answer) ? "no" : "yes";
-            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + correctanswer + "'.\n"
-                    + "Let's try again, " + name + "!");
+        if ((randomNumber % 2 == 0 && "yes".equals(answer)) || (randomNumber % 2 != 0 && "no".equals(answer))) {
+            System.out.println("Correct!");
+            return true;
         }
+        String correctanswer = "yes".equals(answer) ? "no" : "yes";
+        System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + correctanswer + "'.\n"
+                + "Let's try again, " + name + "!");
         return false;
+
     }
 }
