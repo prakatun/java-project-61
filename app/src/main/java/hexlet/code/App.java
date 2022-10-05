b527431acf993c752a64eb6693067fe82c839cfe
evgzhuravlev <evgzhuravlev@yandex-team.ru>
2022-10-05 17:09:59 +0300

    mind_games_6(update_9)

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -11,29 +11,35 @@ import static java.lang.Math.random;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (true) {
+        String numberMenu = "";
+        while (!"0".equals(numberMenu)) {
             String greet =
-                    "Please enter the game number and press Enter.\n"
-                            + "1 - Greet\n"
-                            + "2 - Even\n"
-                            + "3 - Calc\n"
-                            + "0 - Exit";
+                    """
+                            Please enter the game number and press Enter.
+                            1 - Greet
+                            2 - Even
+                            3 - Calc
+                            0 - Exit""";
+
             System.out.println(greet);
             String selectNumber = sc.next();
             System.out.println("Your choice: " + selectNumber);
-            if ("1".equals(selectNumber)) {
-                Cli.sayHello(sc);
-            } else if ("2".equals(selectNumber)) {
-                String name = Cli.sayHello(sc);
-                CheckForEven.game(sc, name);
-            } else if ("3".equals(selectNumber)) {
-                String name = Cli.sayHello(sc);
-                Calculator.game(sc, name);
-            } else if ("0".equals(selectNumber)) {
-                break;
-            }
+            numberMenu = selectNumber;
+            selectGame(sc, selectNumber);
         }
         sc.close();
+    }
+
+    public static void selectGame(Scanner sc, String selectNumber) {
+        if ("1".equals(selectNumber)) {
+            Cli.sayHello(sc);
+        } else if ("2".equals(selectNumber)) {
+            String name = Cli.sayHello(sc);
+            CheckForEven.game(sc, name);
+        } else if ("3".equals(selectNumber)) {
+            String name = Cli.sayHello(sc);
+            Calculator.game(sc, name);
+        }
     }
 
     public static void noCorrectAnswerPrint(String answer, String correctanswer, String name) {
@@ -59,7 +65,7 @@ public class App {
     public static void gameStep(Scanner sc, String name, String gameName) {
         final int step = 3; //кол-во шагов в играх
         int i = 0;
-        boolean resultGames = false;
+        boolean resultGames;
         do {
             i++;
             resultGames = App.resultGames(sc, name, gameName);
